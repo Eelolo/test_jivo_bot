7ae44439220a56686ff6a279663d10507cfc8e43
Eelolo <Eelolo@yandex.ru>
2022-01-23 15:31:13 +0300

    test

--- bot/cadfem_jivo_bot/bot.py
+++ bot/cadfem_jivo_bot/bot.py
@@ -31,8 +31,10 @@ class Bot:
             case = case(self.message_text)
 
             if case:
+                print(f'old {self.chat.step}')
                 print(case['right_away'])
                 self.set_chat_step(case['next_step'])
+                print(f'new {self.chat.step}')
                 step = self.steps[self.chat.step](**self.kwargs)
                 self.process_answer(step)
                 if case['right_away']:
