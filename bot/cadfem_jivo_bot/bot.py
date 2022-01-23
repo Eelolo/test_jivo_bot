7ed6f8c077ea2640f4a8dd6ce7a64e462d319866
Eelolo <Eelolo@yandex.ru>
2022-01-23 16:44:33 +0300

    test

--- bot/cadfem_jivo_bot/bot.py
+++ bot/cadfem_jivo_bot/bot.py
@@ -34,9 +34,9 @@ class Bot:
 
                 if case:
                     self.chat.step = case['next_step']
+                    self.chat.step_in_process = False
                     self.chat.save()
-                    step = self.steps[self.chat.step](**self.kwargs)
-                    self.process_answer(step)
+                    self.process_step()
                     if case['right_away']:
                         self.process_step()
                     break
