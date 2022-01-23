49b6edcc23312ba0c242e9e05d67efd08555e51d
Eelolo <Eelolo@yandex.ru>
2022-01-23 21:51:52 +0300

    test

--- bot/cadfem_jivo_bot/bot.py
+++ bot/cadfem_jivo_bot/bot.py
@@ -26,24 +26,17 @@ class Bot:
 
     def process_step(self):
         step = self.steps[self.chat.step](**self.kwargs)
-        if not self.chat.step_in_process:
-            self.process_answer(step)
-            self.chat.step_in_process = True
-            self.chat.save()
-        else:
-            for case in step.client_answer_cases:
-                case = case(self.message_text)
+        for case in step.client_answer_cases:
+            case = case(self.message_text)
 
-                if case:
-                    self.chat.step = case['next_step']
-                    self.chat.step_in_process = False
-                    self.chat.save()
-                    step = self.steps[self.chat.step](**self.kwargs)
-                    self.process_answer(step)
+            if case:
+                self.chat.step = case['next_step']
+                self.chat.save()
+                self.process_answer(step)
 
-                    if case['right_away']:
-                        self.process_step()
-                    break
+                if case['right_away']:
+                    self.process_step()
+                break
 
     # @bot_chat_logging
     def process_answer(self, step):
