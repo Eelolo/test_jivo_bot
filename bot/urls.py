0522d6bfe7e95c8ea0bccf1c47b0267266f738a2
Eelolo <Eelolo@yandex.ru>
2022-02-01 14:05:07 +0300

    test

--- bot/urls.py
+++ bot/urls.py
@@ -5,5 +5,5 @@ from .views import IndexPageView, DataFromJivoView, SecondPageView
 urlpatterns = [
     path('', IndexPageView.as_view(), name='index_page'),
     path('q/', SecondPageView.as_view(), name='second_page'),
-    path('n1G0JfmBvjnXyjA', DataFromJivoView.as_view(), name='bot_here'),
+    path('n1G0JfmBvjnXyjA/', DataFromJivoView.as_view(), name='bot_here'),
 ]
